52d8660c6f8c201f9526ad507ef7b36d8df6cc11
Xenatal <natalyya.exe@gmail.com>
2021-03-23 10:21:28 +0100

    akt

--- js/script.js
+++ js/script.js
@@ -1,32 +1,30 @@
 $('.review-cards').slick({
-    infinite: false,
-    speed: 300,
     slidesToShow: 4,
-    slidesToScroll: 4,
+    slidesToScroll: 1,
+    infinite: false,
+    arrows: false,
     responsive: [{
-            breakpoint: 1024,
-            settings: {
-                slidesToShow: 3,
-                slidesToScroll: 3,
-                infinite: true,
-            }
-        },
-        {
-            breakpoint: 600,
+            breakpoint: 990,
             settings: {
                 slidesToShow: 2,
-                slidesToScroll: 2
+                slidesToScroll: 1,
+                infinite: true,
+                autoplay: true,
+                autoplaySpeed: 2000,
+                arrows: false
             }
         },
         {
-            breakpoint: 480,
+            breakpoint: 768,
             settings: {
                 slidesToShow: 1,
-                slidesToScroll: 1
+                slidesToScroll: 1,
+                infinite: true,
+                autoplay: true,
+                autoplaySpeed: 2000,
+                arrows: false
             }
         }
-        // You can unslick at a given breakpoint now by adding:
-        // settings: "unslick"
-        // instead of a settings object
+
     ]
 });
